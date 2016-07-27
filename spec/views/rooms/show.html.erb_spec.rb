8b3b4fd8fd4054549ccadac489120ee1bef5e01f
akm <akm2000@gmail.com>
2016-07-27 18:24:38 +0900

    Fix spec to pass

--- spec/views/rooms/show.html.erb_spec.rb
+++ spec/views/rooms/show.html.erb_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "rooms/show", type: :view do
+  include Devise::Test::ControllerHelpers
+
+  let(:user){ FactoryGirl.create(:user) }
   let(:organization){ FactoryGirl.create(:organization) }
   before(:each) do
     assign(:organization, organization)
     @room = assign(:room, FactoryGirl.create(:room, organization: organization))
-    @messages = assign(:messages, [FactoryGirl.create(:message, room: @room)])
+    @messages = assign(:messages, [FactoryGirl.create(:message, room: @room, user: user)])
   end
+  around{|ex| User.current(user){ ex.run } }
 
   it "renders attributes in <p>" do
     render
