f35879e75dd3e40b2fae5caf3d9099ec95b9049f
akm <akm2000@gmail.com>
2016-07-27 18:27:34 +0900

    Add comment to migration

--- db/migrate/20160727092550_create_memberships.rb
+++ db/migrate/20160727092550_create_memberships.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 class CreateMemberships < ActiveRecord::Migration[5.0]
   def change
-    create_table :memberships do |t|
-      t.references :organization, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.integer :role
+    create_table :memberships, comment: '所属' do |t|
+      t.references :organization, null: false, foreign_key: true, comment: '組織'
+      t.references :user, null: false, foreign_key: true, comment: 'ユーザー'
+      t.integer :role, null: false, comment: '役割'
 
       t.timestamps
     end
