898108dc8826c9390aff0b9d41eca4f8891833f8
akm <akm2000@gmail.com>
2016-07-27 16:39:16 +0900

    Use valid routing method

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
   let(:user) { FactoryGirl.create(:user) }
+  let(:organization){ FactoryGirl.create(:organization) }
   before{ login_as(user, :scope => :user) }
 
-  describe "GET /rooms" do
+  describe "GET /organizations/:organization_id/rooms" do
     it "works! (now write some real specs)" do
-      get rooms_path
+      get organization_rooms_path(organization)
       expect(response).to have_http_status(200)
     end
   end
