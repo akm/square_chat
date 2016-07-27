6a9272019e5c86718f002d4c6d68db0998e8565d
akm <akm2000@gmail.com>
2016-07-27 14:21:02 +0900

    Fix spec for rooms routing

--- spec/routing/rooms_routing_spec.rb
+++ spec/routing/rooms_routing_spec.rb
@@ -4,35 +4,35 @@ RSpec.describe RoomsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/rooms").to route_to("rooms#index")
+      expect(:get => "/organizations/2/rooms").to route_to("rooms#index", :organization_id => '2')
     end
 
     it "routes to #new" do
-      expect(:get => "/rooms/new").to route_to("rooms#new")
+      expect(:get => "/organizations/2/rooms/new").to route_to("rooms#new", :organization_id => '2')
     end
 
     it "routes to #show" do
-      expect(:get => "/rooms/1").to route_to("rooms#show", :id => "1")
+      expect(:get => "/organizations/2/rooms/1").to route_to("rooms#show", :organization_id => '2', :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/rooms/1/edit").to route_to("rooms#edit", :id => "1")
+      expect(:get => "/organizations/2/rooms/1/edit").to route_to("rooms#edit", :organization_id => '2', :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/rooms").to route_to("rooms#create")
+      expect(:post => "/organizations/2/rooms").to route_to("rooms#create", :organization_id => '2')
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/rooms/1").to route_to("rooms#update", :id => "1")
+      expect(:put => "/organizations/2/rooms/1").to route_to("rooms#update", :organization_id => '2', :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/rooms/1").to route_to("rooms#update", :id => "1")
+      expect(:patch => "/organizations/2/rooms/1").to route_to("rooms#update", :organization_id => '2', :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/rooms/1").to route_to("rooms#destroy", :id => "1")
+      expect(:delete => "/organizations/2/rooms/1").to route_to("rooms#destroy", :organization_id => '2', :id => "1")
     end
 
   end
