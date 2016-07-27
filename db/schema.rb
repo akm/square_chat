ca950599bd4aec8244525a25539781f134ac0bc4
akm <akm2000@gmail.com>
2016-07-27 10:29:59 +0900

    bin/rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20160725093138) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", comment: "ユーザ" do |t|
+    t.string   "email",                  default: "", null: false, comment: "メールアドレス"
+    t.string   "encrypted_password",     default: "", null: false, comment: "暗号化済みパスワード"
+    t.string   "reset_password_token",                             comment: "パスワードトークン"
+    t.datetime "reset_password_sent_at",                           comment: "パスワードトークン送信日時"
+    t.datetime "remember_created_at",                              comment: "ログイン記憶日時"
+    t.integer  "sign_in_count",          default: 0,  null: false, comment: "ログイン回数"
+    t.datetime "current_sign_in_at",                               comment: "現在ログイン日時"
+    t.datetime "last_sign_in_at",                                  comment: "前回ログイン日時"
+    t.string   "current_sign_in_ip",                               comment: "現在ログインIP"
+    t.string   "last_sign_in_ip",                                  comment: "前回ログインIP"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
