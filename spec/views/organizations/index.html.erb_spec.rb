e5ad75a65285efe8be8bf1ddf5d47603e7b95164
akm <akm2000@gmail.com>
2016-07-27 13:57:43 +0900

    Fix index view spec

--- spec/views/organizations/index.html.erb_spec.rb
+++ spec/views/organizations/index.html.erb_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe "organizations/index", type: :view do
   before(:each) do
     assign(:organizations, [
-      FactoryGirl.create(:organization),
-      FactoryGirl.create(:organization),
+      FactoryGirl.create(:organization, name: 'org1'),
+      FactoryGirl.create(:organization, name: 'org2'),
     ])
   end
 
   it "renders a list of organizations" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "org1", :count => 1
+    assert_select "tr>td", :text => "org2", :count => 1
   end
 end
--- spec/views/rooms/index.html.erb_spec.rb
+++ spec/views/rooms/index.html.erb_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "rooms/index", type: :view do
+  let(:organization){ FactoryGirl.create(:organization) }
   before(:each) do
     assign(:rooms, [
-      FactoryGirl.create(:room),
-      FactoryGirl.create(:room),
+      FactoryGirl.create(:room, organization: organization, name: 'room1'),
+      FactoryGirl.create(:room, organization: organization, name: 'room2'),
     ])
   end
 
   it "renders a list of rooms" do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "room1", :count => 1
+    assert_select "tr>td", :text => "room2", :count => 1
   end
 end
