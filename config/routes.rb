f07cfcf8e85ce5801f5bbcf6f0fa2e7f3aae70e8
akm <akm2000@gmail.com>
2016-07-27 11:09:10 +0900

    Add organization resources path and modify root to its index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :organizations
+
   devise_for :users
   # root to: "devise/sessions#new" # Sign in
-  root to: "rails_admin/main#dashboard" # TODO Change top page
+  root to: "organizations#index" # TODO Change top page
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
