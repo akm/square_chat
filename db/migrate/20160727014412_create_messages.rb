8f13daade8e2db0980d86d427651c6d35d7a7d9a
akm <akm2000@gmail.com>
2016-07-27 10:48:45 +0900

    Add comment to migration

--- db/migrate/20160727014412_create_messages.rb
+++ db/migrate/20160727014412_create_messages.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
-    create_table :messages do |t|
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.text :content
+    create_table :messages, comment: 'メッセージ' do |t|
+      t.references :room, null: false, foreign_key: true, comment: 'ルーム'
+      t.references :user, null: false, foreign_key: true, comment: 'ユーザ'
+      t.text :content, comment: '内容'
 
       t.timestamps
     end
