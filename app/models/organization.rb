f96090adf46a0ec64fd2f49e8936ae24b26a06e8
akm <akm2000@gmail.com>
2016-08-01 18:41:50 +0900

    Fill Membership#name on create an organization owner

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,6 +5,6 @@ class Organization < ApplicationRecord
   after_create :create_owner_membership
   def create_owner_membership
     return unless User.current_user
-    memberships.create!(user: User.current_user, role: :owner)
+    memberships.create!(user: User.current_user, role: :owner, name: User.current_user.email.split('@',2).first)
   end
 end
