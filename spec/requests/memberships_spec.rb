d4fdd1942ffcfb75ef7fd2ec6f54c7cf68c5db3d
akm <akm2000@gmail.com>
2016-08-02 16:08:18 +0900

    Fix path method

--- spec/requests/memberships_spec.rb
+++ spec/requests/memberships_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Memberships", type: :request do
+  let(:organization) { FactoryGirl.create(:organization) }
   let(:user) { FactoryGirl.create(:user) }
   before{ login_as(user, :scope => :user) }
 
   describe "GET /memberships" do
     it "works! (now write some real specs)" do
-      get memberships_path
+      get organization_memberships_path(organization)
       expect(response).to have_http_status(200)
     end
   end
