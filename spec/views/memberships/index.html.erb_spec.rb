6d3039a02a8de926a0bf0d55137f5d570b2382c7
akm <akm2000@gmail.com>
2016-08-02 16:06:06 +0900

    Fix app/views/memberships/index.htmlerb and its spec

--- spec/views/memberships/index.html.erb_spec.rb
+++ spec/views/memberships/index.html.erb_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "memberships/index", type: :view do
 
+  let(:organization){ FactoryGirl.create(:organization) }
   let(:user1){ FactoryGirl.create(:user, email: 'user1@example.com') }
   let(:user2){ FactoryGirl.create(:user, email: 'user2@example.com') }
 
   before(:each) do
+    assign(:organization, organization)
     assign(:memberships, [
-      FactoryGirl.create(:membership, user: user1, name: 'member1'),
-      FactoryGirl.create(:membership, user: user2, name: 'member2'),
+      FactoryGirl.create(:membership, user: user1, name: 'member1', organization: organization),
+      FactoryGirl.create(:membership, user: user2, name: 'member2', organization: organization),
     ])
   end
 
