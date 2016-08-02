92f70cdcf970e16d23d9108dfe928d3c09de2036
akm <akm2000@gmail.com>
2016-08-02 16:03:02 +0900

    Fix app/views/memberships/_form.html.erb and its specs

--- spec/views/memberships/edit.html.erb_spec.rb
+++ spec/views/memberships/edit.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "memberships/edit", type: :view do
+  let(:organization){ FactoryGirl.create(:organization) }
   before(:each) do
-    @membership = assign(:membership, FactoryGirl.create(:membership))
+    assign(:organization, organization)
+    @membership = assign(:membership, FactoryGirl.create(:membership, organization: organization))
   end
 
   it "renders the edit membership form" do
     render
 
-    assert_select "form[action=?][method=?]", membership_path(@membership), "post" do
+    assert_select "form[action=?][method=?]", organization_membership_path(organization, @membership), "post" do
 
       assert_select "input#membership_organization_id[name=?]", "membership[organization_id]"
 
--- spec/views/memberships/new.html.erb_spec.rb
+++ spec/views/memberships/new.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "memberships/new", type: :view do
+  let(:organization){ FactoryGirl.create(:organization) }
   before(:each) do
-    assign(:membership, FactoryGirl.build(:membership))
+    assign(:organization, organization)
+    assign(:membership, FactoryGirl.build(:membership, organization: organization))
   end
 
   it "renders new membership form" do
     render
 
-    assert_select "form[action=?][method=?]", memberships_path, "post" do
+    assert_select "form[action=?][method=?]", organization_memberships_path(organization), "post" do
 
       assert_select "input#membership_organization_id[name=?]", "membership[organization_id]"
 
