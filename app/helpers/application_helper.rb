dddf2e30848e8873d4b8fe8b754ea403f37cb922
akm <akm2000@gmail.com>
2016-08-03 14:01:16 +0900

    Use button group instead of nav-list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def button_link_to(title, href, options = {})
+    options[:class] = [options[:class], 'button_link'].compact.join(' ')
+    options[:type] = 'button'
+    options[:'data-href'] = href
+    content_tag(:button, title, options)
+  end
 end
