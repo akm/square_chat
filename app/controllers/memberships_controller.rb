4c2d1ad526dee3d759beae393539c4882644c3e7
akm <akm2000@gmail.com>
2016-08-03 10:39:51 +0900

    Use user_email instead of user_id in membership form

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -79,6 +79,6 @@ class MembershipsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def membership_params
-      params.require(:membership).permit(:organization_id, :user_id, :name, :role)
+      params.require(:membership).permit(:organization_id, :user_email, :name, :role)
     end
 end
