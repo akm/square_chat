a71230bca95a21d3a7c78aad4deeb994ff1df231
akm <akm2000@gmail.com>
2016-08-03 14:29:39 +0900

    Set btn-primary to the button selected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def button_link_to(title, href, options = {})
+    yield(options) if block_given?
     options[:class] = [options[:class], 'button_link'].compact.join(' ')
     options[:type] = 'button'
     options[:'data-href'] = href
