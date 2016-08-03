934b31a06b77b6d68cb0a146b97aee35b4b15545
akm <akm2000@gmail.com>
2016-08-03 17:49:12 +0900

    Integrate migrations

--- db/migrate/20160727014000_create_memberships.rb
+++ db/migrate/20160727014000_create_memberships.rb
@@ -4,6 +4,7 @@ class CreateMemberships < ActiveRecord::Migration[5.0]
     create_table :memberships, comment: '所属' do |t|
       t.references :organization, null: false, foreign_key: true, comment: '組織'
       t.references :user, null: false, foreign_key: true, comment: 'ユーザー'
+      t.string :name, null: false, comment: 'ユーザ名'
       t.integer :role, null: false, comment: '役割'
 
       t.timestamps
