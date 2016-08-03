d97994b0a855c6dee2d9c1bd3faf0708c5dbed3e
akm <akm2000@gmail.com>
2016-08-03 11:01:16 +0900

    Remove unnecessary permission

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -79,6 +79,6 @@ class RoomsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def room_params
-      params.require(:room).permit(:organization_id, :name)
+      params.require(:room).permit(:name)
     end
 end
