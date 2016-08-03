f6ba43eac5930027a1b0ec793745ffe46dcf2b8b
akm <akm2000@gmail.com>
2016-08-03 18:29:24 +0900

    Add seed data to develop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if Rails.env.development?
+
+  users = []
+  pw = 'password'
+  User.with_options(password: pw, password_confirmation: pw) do |u|
+    users = [
+      u.create!(email: 'user1@example.com'),
+      u.create!(email: 'user2@example.com'),
+      u.create!(email: 'user3@example.com'),
+    ]
+  end
+
+  users.each.with_index(1) do |user, idx|
+    User.current(user) do
+      Organization.create!(name: "org#{idx}").tap do |org|
+        org.rooms.create!(name: 'general')
+        org.rooms.create!(name: 'random')
+      end
+    end
+  end
+end
