96f60f0b572409a3464d6881db5e4458920169a8
akm <akm2000@gmail.com>
2016-08-03 16:52:02 +0900

    Use membership instead of user for message

--- db/migrate/20160727014412_create_messages.rb
+++ db/migrate/20160727014412_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages, comment: 'メッセージ' do |t|
       t.references :room, null: false, foreign_key: true, comment: 'ルーム'
-      t.references :user, null: false, foreign_key: true, comment: 'ユーザ'
+      t.references :membership, null: false, foreign_key: true, comment: 'ユーザ'
       t.text :content, comment: '内容'
 
       t.timestamps
