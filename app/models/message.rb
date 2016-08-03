b733608080bd9cf3cd900fe253db8e66c069e6d8
akm <akm2000@gmail.com>
2016-08-03 16:57:08 +0900

    Use membership instead of user for message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room, required: true
-  belongs_to :user, required: true
+  belongs_to :membership, required: true
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
